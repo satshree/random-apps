129081112c67014f05a50f75e156ea36f98cecfd
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2022-04-28 15:36:21 -0500

    minor ui changes

--- components/HomeButton.js
+++ components/HomeButton.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Button } from "@blueprintjs/core";
 import Link from "next/link";
 
+const mobile = require("is-mobile");
+
 export default class HomeButton extends Component {
   render() {
     return (
       <div
         className="position-absolute w-100"
-        style={{ bottom: "1rem", left: 0 }}
+        style={{ bottom: mobile() ? "2rem" : "1rem", left: 0 }}
       >
         <div className="text-center">
           <Link href="/" passHref={true}>
